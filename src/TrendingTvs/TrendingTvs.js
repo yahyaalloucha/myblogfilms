38f1f7b26cb5e54707b77e6edb45b6e00f95cbc3
Yahya <yahyaallouch01@gmail.com>
2021-07-12 19:02:43 +0300

    nextsteep

--- src/TrendingTvs/TrendingTvs.js
+++ src/TrendingTvs/TrendingTvs.js
@@ -31,23 +31,32 @@ function TrendingTvs() {
         {Data.map((trendtv) => {
           return (
             <div className={style.contents}>
-              <span>
-                <img src={trendtv.poster.asset.url} className={style.poster} />
-              </span>
-              <p
-                key={trendtv.title}
-                style={{
-                  color: "white",
-                }}
-              >
-                {trendtv.title}
-              </p>
-              <p style={{ color: "white" }}>
-                {trendtv.releaseDate.substr(0, 4)}
-              </p>
+              <div className={style.content}>
+                <span>
+                  <img
+                    src={trendtv.poster.asset.url}
+                    className={style.poster}
+                  />
+                </span>
+                <p key={trendtv.title} className={style.tit}>
+                  {trendtv.title}
+                </p>
+                <p className={style.d}>{trendtv.releaseDate.substr(0, 4)}</p>
+              </div>
             </div>
           );
         })}
+      </div>
+      <div className={style.lastdiv}>
+        <footer className={style.footer}>
+          <h2 className={style.footerp}>Vilm © 2021</h2>
+          <p className={style.p}>
+            powered by{" "}
+            <span className={style.biggy} atl="">
+              <a>tmdb</a>
+            </span>
+          </p>
+        </footer>
       </div>
     </div>
   );
