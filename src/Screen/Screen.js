35aa93d92354d7ec7d6bc8bbb6a0b54e9a772c1e
Yahya <yahyaallouch01@gmail.com>
2021-07-12 19:04:59 +0300

    nextsteep

--- src/Screen/Screen.js
+++ src/Screen/Screen.js
@@ -1,6 +1,6 @@
 import style from "./Screen.module.css";
 import React, { useEffect, useState } from "react";
-import j from "./1.jpg";
+
 import Client from "../Client";
 import Vector from "./Vector 2.png";
 import BlockContent from "@sanity/block-content-to-react";
@@ -14,7 +14,6 @@ function Screen() {
         overview,
         releaseDate,
         rating,
-        
       slug,
       poster{asset->{url}}
       }`
@@ -38,9 +37,8 @@ function Screen() {
               <div className={style.Bl}>
                 <BlockContent blocks={screen.overview} />
               </div>
-
               <div className={style.trading}>
-                <img className={style.Vector} src={Vector} />
+                <img className={style.Vector} src={Vector} alt="" />
                 <p className={style.rating}>{screen.rating}</p>
               </div>
             </div>
